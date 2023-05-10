be6d17f2cc7954cd61f0a1591ffc32a9df7b529c
Alex Grover <alexgrover@microsoft.com>
2023-05-10 23:29:39 +0100

    both cards working

---
+++ ServiceNowCard/src/adaptiveCardExtensions/createIncident/quickView/ConfirmView.ts
@@ -0,0 +1,73 @@
+import { ISPFxAdaptiveCard, BaseAdaptiveCardView, ISubmitActionArguments } from '@microsoft/sp-adaptive-card-extension-base';
+import * as strings from 'CreateIncidentAdaptiveCardExtensionStrings';
+import { ICreateIncidentAdaptiveCardExtensionProps, ICreateIncidentAdaptiveCardExtensionState } from '../CreateIncidentAdaptiveCardExtension';
+import { INewIncident, Incident } from '../../../models/IIncident';
+import { HttpClient, IHttpClientOptions } from '@microsoft/sp-http';
+
+export interface IConfirmViewData {
+  ticket: Incident;
+  strings: ICreateIncidentAdaptiveCardExtensionStrings;
+  confirmLink: string;
+  username: string;
+}
+
+export class ConfirmView extends BaseAdaptiveCardView<
+  ICreateIncidentAdaptiveCardExtensionProps,
+  ICreateIncidentAdaptiveCardExtensionState,
+  IConfirmViewData
+> {
+  public get data(): IConfirmViewData {
+    return {
+      ticket: this.state.ticket,
+      strings: strings,
+      username: this.context.pageContext.user.displayName,
+      confirmLink: ""
+    };
+  }
+
+  public get template(): ISPFxAdaptiveCard {
+    return require('./template/ConfirmView.json');
+  }
+
+  public async onAction(action: ISubmitActionArguments): Promise<void> {
+      if (action.type === 'Submit') {
+        const { id } = action.data;
+        if (id === 'cancel') {
+          this.quickViewNavigator.close();
+        } else {
+
+        // send to api
+        await this.CreateTicketInServiceNow()
+
+        this.quickViewNavigator.close();
+        }
+      }
+    }
+
+    private async CreateTicketInServiceNow() : Promise<void> {
+
+      const requestHeaders: Headers = new Headers();
+      requestHeaders.append('Content-type', 'application/json');
+
+      const newIncident: INewIncident = {
+        incident: this.state.ticket
+      };
+  
+      const body: string = JSON.stringify(newIncident);
+  
+      console.log(body)
+  
+      const httpClientOptions: IHttpClientOptions = {
+        body: body,
+        headers: requestHeaders
+      };
+  
+      return await this.context.httpClient.post(
+          `https://ag-viva-connections-servicenow.azurewebsites.net/api/CreateIncidentForUser?upn=${this.context.pageContext.user.email}`,
+          HttpClient.configurations.v1, 
+          httpClientOptions)
+          .then(response => {
+            console.log(response.status)
+          });
+    }
+}
